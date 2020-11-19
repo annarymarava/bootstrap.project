b52bc7c11c57d2c58fc80cc2e9b0986408a0e25b
Anna <arymarava@gmail.com>
2020-11-19 15:54:55 +0300

    Correct mobile version

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,20 @@
-// window.addEventListener("scroll", function() {
-//     var header = document.querySelector("header");
-//     header.classList.toggle("sticky", window.scrollY > 320);
-// })
+
+let burgerIcon = document.querySelector('navbar-toggler');
+let body = document.querySelector('body');
+let headerListSubstrate = document.querySelector('.header-list-substrate');
+
+
+burgerIcon.addEventListener('click', function () {
+//   burgerMenu.classList.remove('close');
+//   headerListSubstrate.classList.remove('close');
+  body.classList.toggle('hidden-scroll');
+});
+
+// headerListSubstrate.addEventListener('click', function () {
+//   burgerMenu.classList.remove('active');
+//   headerListSubstrate.classList.remove('active');
+//   html.classList.remove('hidden-scroll');
+//   body.style.paddingRight = 0;
+//   burgerMenu.classList.add('close');
+//   headerListSubstrate.classList.add('close');
+// });
